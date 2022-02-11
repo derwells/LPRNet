ad95a55f331138a50303bb01f354328a9e796091
Derick Wells <wowells@up.edu.ph>
2022-02-11 18:43:22 +0800

    Fix config typo

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ CCPD_PATH = "/home/derick/Downloads/CCPD/CCPD2019"
 
 # List of file splits
 TRAIN_PATH = os.path.join(CCPD_PATH, "splits/train.txt")
-VAL_PATH = os.path.join(CCPD_PATH, "splis/val.txt")
+VAL_PATH = os.path.join(CCPD_PATH, "splits/val.txt")
 TEST_PATH = os.path.join(CCPD_PATH, "splits/test_easy.txt")
