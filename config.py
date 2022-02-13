4f20497d0cfae471264c86dbd294562b868ceb67
Derick Wells <wowells@up.edu.ph>
2022-02-13 21:10:27 +0800

    Add split dict to constants

--- config.py
+++ config.py
@@ -7,6 +7,14 @@ CCPD_PATH = "/home/derick/Downloads/CCPD/CCPD2019"
 TRAIN_PATH = os.path.join(CCPD_PATH, "splits/train.txt")
 VAL_PATH = os.path.join(CCPD_PATH, "splits/val.txt")
 TEST_PATH = os.path.join(CCPD_PATH, "splits/test_easy.txt")
+RAW_SPLIT_PATHS = {
+    "test": TEST_PATH,
+    "val": VAL_PATH,
+    "train": TRAIN_PATH
+}
+
+# Clean data directory
+DATA_PATH = "data/"
 
 # Training
 INPUT_DIMS = (24, 94, 3)
